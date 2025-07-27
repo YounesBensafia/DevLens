c660a918d05ec7d2954f04b6c088cb575acda47e
YounesBensafia <younes.bensafia@etu.usthb.dz>
2025-07-27 18:54:02 +0100

    .f

--- devlens/utils/structure_counts.py
+++ devlens/utils/structure_counts.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def get_gitignore_patterns(directory: str = "."):
     current_dir = Path(directory).resolve()
 
-    while current_dir != current_dir.parent:  #  at the root directory
+    while current_dir != current_dir.parent:  #  at 
         gitignore_path = current_dir / '.gitignore'
         if gitignore_path.exists():
             with open(gitignore_path, 'r') as f:
