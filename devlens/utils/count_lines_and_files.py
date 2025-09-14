af15ec544dba37920a13e5d9dba8b47ed8010b92
Younes Bensafia <younes.bensafia@outlook.com>
2025-09-14 19:51:14 +0100

    feat: improve line counting logic; enhance file handling and return format for language detection

--- devlens/utils/count_lines_and_files.py
+++ devlens/utils/count_lines_and_files.py
@@ -4,22 +4,29 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config.settings import SUPPORTED_FILE_TYPES
 from utils.structure_the_project import list_non_ignored_files
 
+files = []
 
 def get_language(file_path: str) -> str:
     split_name = os.path.splitext(file_path)
     for extension in split_name:
         if extension in SUPPORTED_FILE_TYPES.keys():
             return SUPPORTED_FILE_TYPES[extension]
-    return "Not A Language"
+    return "N/A"
 
 def count_lines_in_file(content: str) -> int:
     return sum(1 for _ in content.splitlines())
 
 def count_lines_by_language(files: list[str]):
-    for file in files:
+    abs_files = [os.path.abspath(file) for file in files]
+    for file in abs_files:
         if os.path.exists(file):
             lang = get_language(file)
-            print(f"{file}: {lang}")
+            if lang != "N/A":
+                content = get_content_of_file(file)
+                if content:
+                    line_count = count_lines_in_file(content)
+                    files.append((line_count, lang))
+    return files
 
 def get_content_of_file(file_path: str) -> str:
     with open(file_path, "r", encoding="utf-8", errors="ignore") as f:
@@ -32,4 +39,4 @@ def get_content_of_file(file_path: str) -> str:
                 non_comment_lines.append(line)
         return '\n'.join(non_comment_lines)
 
-count_lines_by_language(list_non_ignored_files("devlens"))
+print(count_lines_by_language(list_non_ignored_files("devlens")))
